5b284d30d01305848777687041c101ac0ccf2a92
khunhour <khonhourmorm@gmail.com>
2022-02-17 16:54:02 +0900

    made skeleton of main header footer and import images and target img

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Main.css"
+export default function Main() {
+  return (
+    <div className='img-container'>
+        <img src='/images/cartoonnetwork.jpg' alt="pic"/>
+    </div>
+  )
+}
