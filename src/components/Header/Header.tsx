d361ffaf65d0034d29ee0d4e101a6a76e830c570
Khunhour Morm <khonhourmorm@gmail.com>
2022-02-18 18:53:16 +0900

    added abit styling on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./Header.css"
+import React from "react";
+import "./Header.css";
 
 export default function Header() {
-  return (
-    <header>
-      <div className='logo'>Seek&Find</div>
-      <div className='timer'></div>
-      <div className='target'></div>
-    </header>
-  )
+	return (
+		<header>
+			<div className="logo">Seek&Find</div>
+			<div className="timer">Timer</div>
+			<div className="target">target</div>
+		</header>
+	);
 }
