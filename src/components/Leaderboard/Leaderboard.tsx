4e5910a79dce8839ed48894356a7b6229b09b378
Khunhour Morm <khonhourmorm@gmail.com>
2022-02-22 18:11:01 +0900

    added leader and table to it and styling

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import "./Leaderboard.css";
 
 type Props = {};
 
 const Leaderboard = (props: Props) => {
-	return <div>Leaderboard</div>;
+	return (
+		<div id="leaderboard">
+			<h2>Leaderboard</h2>
+			<div className="ranking">
+				<h3>Ranking</h3>
+				<table>
+					<tr>
+						<th>Rank</th>
+						<th>Name</th>
+						<th>Time</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Mary Poppins</td>
+						<td>30s</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Mary Poppins</td>
+						<td>30s</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Mary Poppins</td>
+						<td>30s</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Mary Poppins</td>
+						<td>30s</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	);
 };
 
 export default Leaderboard;
