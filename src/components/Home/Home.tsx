0e30de27bf8af63dc3d08224cc0e1bd9cf7681de
Khunhour Morm <khonhourmorm@gmail.com>
2022-02-22 19:37:37 +0900

    edit wording and add margin top to footer

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -8,14 +8,14 @@ const Home = (props: Props) => {
 			<h2>Seek&Find</h2>
 			<div className="objective">
 				<h3>Objective</h3>
-				<div>Find all 3 characters as fast as possible!</div>
+				<div>Find all 3 cartoon characters as fast as possible!</div>
 			</div>
 			<div className="how-to-play">
 				<h3>How to Play</h3>
 				<ol>
 					<li>Enter your name and click "Play".</li>
 					<li>The timer will start as soon as the image loads.</li>
-					<li>Select the location on the image.</li>
+					<li>Click a location on the image.</li>
 					<li>Choose the character you think you have found.</li>
 					<li>The game will end when all 3 characters are found.</li>
 					<li>See your scores on the Leaderboard.</li>
