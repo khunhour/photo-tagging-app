1a06afefd10e0871c7d03229aacc15516c365620
Khunhour Morm <khonhourmorm@gmail.com>
2022-02-26 23:57:58 +0900

    created button with mui on leaderboard component

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -4,7 +4,9 @@ import "./Leaderboard.css";
 import { Link } from "react-router-dom";
 import { LeaderboardType } from "../../type/LeaderboardType";
 import { formatSeconds } from "../../utilities/formatSeconds";
-
+import { Button } from "@material-ui/core";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 type Props = {
 	leaderboard: LeaderboardType | undefined;
 	handleGameStart: (e: any) => void;
@@ -17,9 +19,21 @@ const Leaderboard: React.FC<Props> = ({ handleGameStart, leaderboard }) => {
 				<h1>Leaderboard</h1>
 				<div className="button-container">
 					<Link to="/">
-						<button>{"<"} Back Home</button>
+						<Button
+							variant="contained"
+							startIcon={<ArrowBackIosIcon />}
+						>
+							{" "}
+							Back Home
+						</Button>
 					</Link>
-					<button onClick={handleGameStart}>Play Again {">"}</button>
+					<Button
+						variant="contained"
+						endIcon={<ArrowForwardIosIcon />}
+						onClick={handleGameStart}
+					>
+						Play Again
+					</Button>
 				</div>
 				<div className="ranking">
 					<h2>Ranking</h2>
