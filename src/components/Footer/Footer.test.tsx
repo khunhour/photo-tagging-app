01fa1c73f9f5ec25486f989e522fe304a9ff8084
Khunhour Morm <khonhourmorm@gmail.com>
2022-03-07 04:49:23 +0900

    change test name in footer test

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Footer from "./Footer";
 
-it("renders result div", () => {
+it("renders footer div", () => {
 	render(<Footer />);
 	const footer = screen.getByTestId("footer");
 	expect(footer).toBeInTheDocument();
