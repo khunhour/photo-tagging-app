4fede331e9b124bf6f4cff0a47e50979bb2b1439
Khunhour Morm <khonhourmorm@gmail.com>
2022-03-06 20:04:08 +0900

    add scss for app and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
+import "./App.css";
+
 //firebase
 import firebase from "./firebase/firebase";
 
