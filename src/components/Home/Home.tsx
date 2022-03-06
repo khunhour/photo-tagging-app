623b0c8328ca1efdee5e3bdb2ecffde3516994f5
Khunhour Morm <khonhourmorm@gmail.com>
2022-03-06 20:41:15 +0900

    style home component using scss

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "./Home.css";
+import "./Home.css";
+
 type Props = {
 	currentPlayer: string;
 	handleNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -13,44 +14,36 @@ const Home: React.FC<Props> = ({
 	handleGameStart,
 }) => {
 	return (
-		<div className="wrapper">
-			<div id="home">
-				<h1>Seek&Find</h1>
-				<div className="objective">
-					<h2>Objective</h2>
-					<div>
-						Find all 3 cartoon characters as fast as possible!
-					</div>
-				</div>
-				<div className="how-to-play">
-					<h2>How to Play</h2>
-					<ol>
-						<li>Enter your name and click "Play".</li>
-						<li>
-							The timer will start as soon as the image loads.
-						</li>
-						<li>Click a location on the image.</li>
-						<li>Choose the character you think you have found.</li>
-						<li>
-							The game will end when all 3 characters are found.
-						</li>
-						<li>See your scores on the Leaderboard.</li>
-					</ol>
-				</div>
-				<form onSubmit={handleGameStart}>
-					<h2>Enter Your Name</h2>
-					<input
-						placeholder="Peter Parker"
-						required
-						onChange={handleNameChange}
-						value={currentPlayer}
-					/>
-					<button type="submit">Play</button>
-				</form>
-				<Link to="/leaderboard">
-					<button>Leaderboard</button>
-				</Link>
+		<div id="home" className="wrapper">
+			<h1>Seek&Find</h1>
+			<div className="objective">
+				<h2>Objective</h2>
+				<div>Find all 3 cartoon characters as fast as possible!</div>
 			</div>
+			<div className="how-to-play">
+				<h2>How to Play</h2>
+				<ol>
+					<li>Enter your name and click "Play".</li>
+					<li>The timer will start as soon as the image loads.</li>
+					<li>Click a location on the image.</li>
+					<li>Choose the character you think you have found.</li>
+					<li>The game will end when all 3 characters are found.</li>
+					<li>See your scores on the Leaderboard.</li>
+				</ol>
+			</div>
+			<form onSubmit={handleGameStart}>
+				<h2>Enter Your Name</h2>
+				<input
+					placeholder="Peter Parker"
+					required
+					onChange={handleNameChange}
+					value={currentPlayer}
+				/>
+				<button type="submit">Play</button>
+			</form>
+			<Link to="/leaderboard">
+				<button>Leaderboard</button>
+			</Link>
 		</div>
 	);
 };
